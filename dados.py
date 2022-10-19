6cc7572c55754c2cf5f90c43f9f156d0d1b27dbf
Juniooor <juniormodern@hotmail.com>
2022-10-18 21:20:28 -0300

    finalizando logica POO

--- dados.py
+++ dados.py
@@ -11,12 +11,9 @@ class Dadosbd:
             database='bdagenda'
         )
 
-        self.cursor = self.conexao.cursor()   
-        
-             
-        
-        
-    def inserir(self,nome, numero):
+        self.cursor = self.conexao.cursor()
+
+    def inserir(self, nome, numero):
         try:
             comando = f'INSERT INTO agenda(nome, numero) VALUES ("{nome}", "{numero}")'
             self.cursor.execute(comando)
@@ -24,20 +21,38 @@ class Dadosbd:
             print('usuario cadastrado')
         except Error as e:
             return f'error {e} verifique sql'
+
+    
+    def excluir(self, idagenda):
+        comando = f'DELETE FROM agenda WHERE idagenda = "{idagenda}"'
+        self.cursor.execute(comando)
+        self.conexao.commit()
+        print('numero apagado')
         
+    def editar(self, numero, nome, idagenda):
+        comando = f'UPDATE agenda SET numero = "{numero}", nome = "{nome}" WHERE idagenda = "{idagenda} LIMIT 1" '
+        self.cursor.execute(comando)
+        self.conexao.commit()
+        print('dados atualizados')
         
+    def vizualizar(self, nome):
+        comando = f'SELECT * FROM agenda WHERE nome LIKE "{nome}%"'
+        self.cursor.execute(comando)
+        resultado = self.cursor.fetchall()
+        return resultado
+
     # def __del__(self):
     #     self.cursor.close()
     #     self.conexao.close()
 # import mysql.connector
 
 
+if __name__ == "__main__":
 
-
-        
-if __name__ == "__main__" :
-    
     cadastro = Dadosbd()
-    cadastro.inserir(nome='junior3', numero='12345678')
+    dados = cadastro.vizualizar(nome='junior')
     
-      
+    for i, e , a in dados:
+        print(f' {i} |  {e}   |   {a}')
+    
+    
