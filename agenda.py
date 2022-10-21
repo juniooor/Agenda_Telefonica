7fb0aad586378044f3f521ae822cac41a974fac1
Juniooor <juniormodern@hotmail.com>
2022-10-20 21:05:51 -0300

    finalizando projeto

--- agenda.py
+++ agenda.py
@@ -29,7 +29,6 @@ while True:
                 numero = input(' insira o numero do contato: ')
 
         print('inserindo contato')
-
     elif opcao == '2':
         contato = input('Qual o nome do contato que você quer excluir?: ')
         vizualizar = funcao.vizualizar(contato)
@@ -44,6 +43,27 @@ while True:
         funcao.excluir(ids)
         print('excluindo contato...')
     elif opcao == '3':
+        contato = input('Qual o nome do contato que você quer editar?: ')
+        vizualizar = funcao.vizualizar(contato)
+        
+        while True:
+            try:
+                ids = int(input(f'\n Qual ID do contato que você quer editar?: '))
+                if isinstance(ids, int) == True:
+                    break
+            except ValueError as e:
+                print('você informou um id invalido por favor digite corretamente ')
+        nome = input('Insira o nome do contato: ')
+        numero = input(' insira o numero do contato: ')
+        while True:
+            confirme = input(f'você confirma nome: {nome} e numero: {numero} ?[SIM/NAO] ').upper()
+            if confirme == 'SIM':
+                funcao.inserir(nome, numero)
+                break
+            else:
+                nome = input('Insira o nome do contato: ')
+                numero = input(' insira o numero do contato: ')
+        funcao.editar(nome=nome, numero=numero, idagenda=ids)
         print('editando contato')
     elif opcao == '4':
         nome = input('Informe o nome do contato:  ')
